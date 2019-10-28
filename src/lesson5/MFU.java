a9d56a106ad5c84808f5272ff1a9a8559add9245
Rustamkvk <Rustamkvk@gmail.com>
2019-10-28 19:34:03 +0300

    12

---
+++ src/lesson5/MFU.java
@@ -0,0 +1,55 @@
+package lesson5;
+
+public class MFU {
+
+
+    private  final Object mon = new Object();
+    private volatile char currentLetter = 'A';
+
+    public static void main(String[] args) {
+      MFU w = new MFU();
+        Thread t1 = new Thread(() -> {
+            w.printA();
+        });
+        Thread t2 = new Thread(() -> {
+            w.printB();
+        });
+        t1.start();
+        t2.start();
+    }
+
+    public void printA() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'A') {
+                        mon.wait();
+                    }
+                    System.out.print("A");
+                    currentLetter = 'B';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void printB() {
+        synchronized (mon) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    while (currentLetter != 'B') {
+                        mon.wait();
+                    }
+                    System.out.print("B");
+                    currentLetter = 'A';
+                    mon.notify();
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
